0de87416cc7ae5e0b2d18c99bcf5ad83649e5a92
LoannBenoit <loann.benoit@hotmail.com>
2020-11-19 11:42:14 +0100

    Add focus/blur behavior

--- 4cam/src/components/Search.component.vue
+++ 4cam/src/components/Search.component.vue
@@ -1,33 +1,64 @@
 <template>
-    <div class="search">
-        <input name="searchbar" id="searchbar" placeholder="Rechercher une enseigne...">
-    </div>
+  <div class="search">
+    <input
+      name="searchbar"
+      id="searchbar"
+      placeholder="Rechercher une enseigne..."
+      @click="changeBackground()"
+      v-model="value"
+      @blur="focusOut"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'searchbar',
-}
+  name: "searchbar",
+  data() {
+    return {
+      focused: false,
+    };
+  },
+  props: {
+    value: {
+      type: String,
+      required: false,
+    },
+  },
+  watch: {
+    value() {
+      this.$emit("input", this.value);
+    },
+  },
+  methods: {
+    changeBackground() {
+      this.$emit("clicked", true);
+      this.focused = true;
+    },
+    focusOut() {
+			this.$emit('blur', true)
+      this.focused = false;
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss">
 .search {
-    width: 100%;
-    position: absolute;
-    margin: 70px auto;
-    
+  width: 100%;
+  position: absolute;
+  margin: 70px auto;
 }
-    #searchbar {
-        height: 50px;
-        width: 90%;
-        top: 0px;
-        padding-left: 10px;
-        border: 1px solid #424BB3;
-        border-radius: 15px;
-    }
-    #searchbar:focus {
-        outline: none;
-        box-shadow: 0px 5px 20px #424bb380;
-    }
+#searchbar {
+  height: 50px;
+  width: 90%;
+  top: 0px;
+  padding-left: 10px;
+  border: 1px solid #424bb3;
+  border-radius: 15px;
+  &:focus {
+    outline: none;
+    box-shadow: 0px 5px 20px #424bb380;
+  }
+}
 </style>
--- 4cam/src/views/HomePage.vue
+++ 4cam/src/views/HomePage.vue
@@ -1,25 +1,43 @@
 <template>
   <div id="main">
-      <head><link href='https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.css' rel='stylesheet' /></head>
-      <BaseMap/>
-      <SearchBar/>
+    <head>
+      <link href="https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.css" rel="stylesheet" />
+    </head>
+    <BaseMap v-if="!focused" />
+    <SearchBar @clicked="onFocus" v-model="search" @blur="onBlur"/>
+    <pre>{{ search }}</pre>
   </div>
 </template>
 
 <script>
-import BaseMap from '@/components/VueMapbox.vue'
-import SearchBar from '@/components/Search.component.vue'
+import BaseMap from "@/components/VueMapbox.vue";
+import SearchBar from "@/components/Search.component.vue";
 export default {
-    components: {
-        SearchBar,
-        BaseMap
+  components: {
+    SearchBar,
+    BaseMap,
+  },
+  data() {
+    return {
+        focused: false,
+        search: null
     }
-}
+  },
+  methods: {
+      onFocus() {
+          this.focused = !this.focused
+          console.log('Je suis focus')
+      },
+      onBlur() {
+        this.focused = !this.focused
+      }
+  },
+};
 </script>
 
 <style>
 #main {
-    width: 100%;
-    display: flex;
+  width: 100%;
+  display: flex;
 }
 </style>
